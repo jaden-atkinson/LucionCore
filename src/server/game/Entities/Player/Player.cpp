b6be72d475a52b03c34b0654db84ba73c5ef0e74
KamiliaBlow <25077548+KamiliaBlow@users.noreply.github.com>
2026-04-10 21:36:04 +0900

    Core/Player - Teleportation code fix
    - Teleportation confirmation occurred earlier than it physically did on the client, so some teleports worked after relogging to the server for the character.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1416,7 +1416,6 @@ bool Player::TeleportTo(TeleportLocation const& teleportLocation, TeleportToOpti
         SetSemaphoreTeleportNear(false);
         //setup delayed teleport flag
         SetDelayedTeleportFlag(IsCanDelayTeleport());
-        SetSemaphoreTeleportFar(true);
         //if teleport spell is cast in Unit::Update() func
         //then we need to delay it until update process will be finished
         if (IsHasDelayedTeleport())
@@ -1427,6 +1426,7 @@ bool Player::TeleportTo(TeleportLocation const& teleportLocation, TeleportToOpti
             m_teleportSpellId = teleportSpellId;
             return true;
         }
+        SetSemaphoreTeleportFar(true);
 
         SetSelection(ObjectGuid::Empty);
 
